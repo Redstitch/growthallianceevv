d219efc29bae5b13ad78df551bf23427195dbecd
Scott Wambach <scott.wambach@redstitchdigital.com>
2019-04-16 09:48:49 -0500

    correct Props

--- src/components/atoms/imgix/Banner.js
+++ src/components/atoms/imgix/Banner.js
@@ -3,36 +3,31 @@ import Imgix, { Picture, Source } from 'react-imgix';
 import imageFixer from '../../../js/imageFixer';
 import { breakpoints } from '../../../styles/utilities/settings';
 import SPicture from '../../../styles/atoms/SPicture';
-
-const commonProps = {
-  imgixParams: {
-    q: '100',
-  },
-};
+import CommonProps from './CommonProps';
 
 const Banner = ({ src }) => (
   <SPicture>
     <Picture>
       <Source
-        {...commonProps}
+        {...CommonProps}
         src={imageFixer(src)}
         width={breakpoints.pageWidth}
         htmlAttributes={{ media: `(min-width: ${breakpoints.ipadLand + 1}px)` }}
       />
       <Source
-        {...commonProps}
+        {...CommonProps}
         src={imageFixer(src)}
         width={breakpoints.ipadLand}
         htmlAttributes={{ media: `(min-width: ${breakpoints.ipadPort + 1}px)` }}
       />
       <Source
-        {...commonProps}
+        {...CommonProps}
         src={imageFixer(src)}
         width={breakpoints.ipadPort}
         htmlAttributes={{ media: `(min-width: ${breakpoints.mobile + 1}px)` }}
       />
       <Source
-        {...commonProps}
+        {...CommonProps}
         src={imageFixer(src)}
         width={breakpoints.mobile}
         htmlAttributes={{ media: `(max-width: ${breakpoints.mobile}px)` }}
