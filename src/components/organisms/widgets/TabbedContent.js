7d2e00b203a7047801a1b9bc30053a16af40c570
Brian Harding <brian.harding@redstitchdigital.com>
2019-05-03 16:22:14 -0500

    Image w/description #224546602 resolves #22

--- src/components/organisms/widgets/TabbedContent.js
+++ src/components/organisms/widgets/TabbedContent.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import Wrapper from '../../../styles/utilities/Wrapper';
-import RichText from './RichText';
 import { button } from '../../../styles/utilities/elements';
 import { colors } from '../../../styles/utilities/settings';
 import { above, below } from '../../../styles/utilities/mediaQueries';
+import fonts from '../../../styles/utilities/fonts';
 
 
 class TabbedContent extends Component {
   state = {
     activeIndex: 0,
+    mobileSelectBox: 'closed',
   }
 
   render() {
     const { widget } = this.props;
-    const { activeIndex } = this.state;
+    const { activeIndex, mobileSelectBox } = this.state;
     return (
       <Wrapper>
         <STabbedContent>
-          <ul>
+          <a
+            className={mobileSelectBox === 'open' ? 'selectbox-toggle open' : 'selectbox-toggle'}
+            href={null}
+            onClick={() => {
+              if (mobileSelectBox === 'closed') {
+                this.setState(() => ({
+                  mobileSelectBox: 'open',
+                }));
+              } else {
+                this.setState(() => ({
+                  mobileSelectBox: 'closed',
+                }));
+              }
+            }}
+          >
+            {widget.tabs[activeIndex].button_text}
+          </a>
+          <ul className={mobileSelectBox}>
             {widget.tabs.map((tab, index) => (
               <li key={`tab${index}`}>
                 <a
@@ -26,6 +44,7 @@ class TabbedContent extends Component {
                   onClick={() => {
                     this.setState(() => ({
                       activeIndex: index,
+                      mobileSelectBox: 'closed',
                     }));
                   }}
                 >
@@ -50,14 +69,43 @@ const STabbedContent = styled.div`
   ${above.ipadPort`
     display: flex;
   `}
+  .selectbox-toggle {
+    display: block;
+    cursor: pointer;
+    ${button};
+    background-color: ${colors.blue};
+    padding: 20px;
+    width: 100%;
+    &.open {
+      &:after {
+        transform: rotate(180deg);
+      }
+    }
+    &:after {
+      ${fonts.FontAwesome};
+      content: '\f078';
+      float: right;
+    }
+    ${above.ipadPort`
+      display: none;
+    `}
+  }
   ul {
     min-width: 25%;
     margin-right: 100px;
     ${below.ipadLand`
       margin-right: 50px;
     `}
+    ${below.ipadPort`
+      padding-top: 5px;
+      margin: -13px 20px 0;
+      display: none;
+      background-color: ${colors.lightBlue};
+      &.open {
+        display: block;
+      }
+    `}
     li {
-      display: inline-block;
       margin: 10px;
       ${above.ipadPort`
         display: block;
@@ -69,24 +117,33 @@ const STabbedContent = styled.div`
         background-color: ${colors.blue};
         text-align: center;
         padding: 20px;
-        ${above.ipadPort`
-          display: block;
+        display: block;
+        ${below.ipadPort`
+        text-align: left;
+          background-color: ${colors.lightBlue};
+          color: ${colors.darkerGray};
         `}
       }
+      ${above.ipadPort`
+        &:nth-child(4n - 3) a {
+          clip-path: polygon(4% 0, 100% 5%, 97% 93%, 0% 100%);
+        }
 
-      &:nth-child(4n - 3) a {
-        clip-path: polygon(4% 0, 100% 5%, 97% 93%, 0% 100%);
-      }
+        &:nth-child(4n - 2) a {
+          clip-path: polygon(2% 3%,98% 6%,100% 94%,0 98%);
+        }
 
-      &:nth-child(4n - 2) a {
-        clip-path: polygon(2% 3%,98% 6%,100% 94%,0 98%);
-      }
-
-      &:nth-child(4n - 1) a {
-        clip-path: polygon(0 0, 100% 0, 98% 98%, 3% 100%);
-      }
+        &:nth-child(4n - 1) a {
+          clip-path: polygon(0 0, 100% 0, 98% 98%, 3% 100%);
+        }
+      `}
     }
   }
+  ${below.ipadPort`
+    .tab-content {
+      margin-top: 30px;
+    }
+  `}
   h3 {
     color: ${colors.blue};
   }
